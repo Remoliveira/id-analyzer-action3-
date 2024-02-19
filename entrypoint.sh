4041137ed31cc03f93d6d9f2aa5710bf522fd692
Remo de Oliveira Gresta <remoogg@gmail.com>
2024-02-19 20:00:15 -0300

    adding results

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,6 @@
 ############!/bin/sh -l
 
 # download and install srcml
-ls
-pwd
-
-
 
 
 #setup directory
@@ -13,8 +9,6 @@ mkdir tempActionFolderIdAnalyzer_v1
 shopt -s extglob
 mv !(tempActionFolderIdAnalyzer_v1) tempActionFolderIdAnalyzer_v1/
 # mv java1.java tempActionFolderIdAnalyzer_v1/
-
-
 
 #convert files to srcml
 srcml --verbose tempActionFolderIdAnalyzer_v1 -o master.xml
@@ -26,9 +20,6 @@ mv /github/workspace/master.xml /
 
 
 python3 CatchIdentifiers.py
-
-#run categories algorithm
-python3 setCategories.py
 
 python3 results.py
 
